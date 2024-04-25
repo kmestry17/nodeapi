262a5affc201de1d398ad087510be3af56a4ae10
Kunal Mestry <kmestry17@gmail.com>
2024-04-24 23:24:16 -0500

    tested each services - CIT signed off

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const bodyParser = require('bodyParser');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const todoRoutes = require('./routes/todoRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,8 +10,19 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 //Routes
+app.use('/todos', todoRoutes);
 
 //Connect to MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/tododatabase', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch((error) => {
+    console.error('MongoDB connection error:', error);
+});
 
 //Start the server
 app.listen(PORT, () => {
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -3,23 +3,73 @@ const Todo = require('../models/Todo');
 
 const router = express.Router();
 
-// Create a new todo
+// Create a new todo list or add todos to an existing list
 router.post('/', async (req, res) => {
     try {
-        const todo = new Todo(req.body);
-        await todo.save();
-        res.status(201).send(todo);
+        const todos = req.body.todos; // Extract the array of todos from the request body
+        const createdTodos = await Todo.insertMany(todos.map(todo => ({ todos: [todo] }))); // Create multiple todos
+        console.log('Created todos:', createdTodos);
+        res.status(201).send(createdTodos);
     } catch (error) {
-        res.send(400).send(error);
+        res.status(400).send(error);
     }
 });
 
-// Get all todos
+// Get all todo lists
 router.get('/', async (req, res) => {
     try {
-        const todo = await Todo.find();
-        res.send(todos)
+        const todoLists = await Todo.find();
+        res.send(todoLists);
     } catch (error) {
         res.status(500).send(error);
     }
-})
+});
+
+// Get a single todo list
+router.get('/:id', async (req, res) => {
+    try {
+        const todoList = await Todo.findById(req.params.id);
+        if (!todoList) {
+            return res.status(404).send();
+        }
+        res.send(todoList);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+// Update a todo list
+router.patch('/:id', async (req, res) => {
+    try {
+        const updatedTodos = req.body.todos; // Extract the array of updated todos from the request body
+
+        const todoList = await Todo.findByIdAndUpdate(
+            req.params.id,
+            { $push: { todos: { $each: updatedTodos } } },
+            { new: true }
+        );
+
+        if (!todoList) {
+            return res.status(404).send();
+        }
+
+        res.send(todoList);
+    } catch (error) {
+        res.status(400).send();
+    }
+});
+
+// Delete a todo list
+router.delete('/:id', async (req, res) => {
+    try {
+        const todoList = await Todo.findByIdAndDelete(req.params.id);
+        if (!todoList) {
+            return res.status(404).send();
+        }
+        res.send(todoList);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+module.exports = router;
